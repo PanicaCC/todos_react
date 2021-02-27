405f57c657b2ec637a6924b27f247c191bf79c08
PanicaCC <panicacc@gmail.com>
2021-02-27 23:44:10 +0200

    Todo list project is Done!

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -4,24 +4,9 @@ import "./Layout.scss"
 const Layout = props => {
     return (
         <div className={"Layout"}>
-            <header className="header">
-                <div className="container">
-                    Header
-                    <hr/>
-                </div>
-
-            </header>
-
             <main>
                 { props.children }
             </main>
-
-            <footer className={"footer"}>
-                <div className="container">
-                    <hr/>
-                    Footer
-                </div>
-            </footer>
         </div>
     )
 }
